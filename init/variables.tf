d98b0dab5ebd9b6c3c6e9b5329aec8f30bb92ec2
Abiyasa <abiyasa2410@gmail.com>
2023-03-28 21:33:46 +0700

    feat(init): main, output, provider, variables

---
+++ init/variables.tf
@@ -0,0 +1,14 @@
+variable "project_code" {
+  default     = "xxx"
+  description = "Project Code"
+}
+
+variable "environment" {
+  default     = "init"
+  description = "Environment"
+}
+
+variable "region" {
+  default     = "ap-southeast-1"
+  description = "AWS Region"
+}
